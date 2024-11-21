08f40c14faabd30126948dfcfd63817a249a850d
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-11-21 16:38:55 +0330

    init-withdraw-method-and-ref BankController

--- src/main/java/org/example/bankapp/controller/BankController.java
+++ src/main/java/org/example/bankapp/controller/BankController.java
@@ -2,13 +2,12 @@ package org.example.bankapp.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.bankapp.model.User;
 import org.example.bankapp.service.BankService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +16,13 @@ public class BankController {
     private final BankService bankService;
 
     @GetMapping("/bank/balance/{bankId}")
-    public ResponseEntity<Long> getBalance(@PathVariable("bankId") UUID bankId) {
+    public ResponseEntity<Double> getBalance(@PathVariable("bankId") UUID bankId) {
         return ResponseEntity.ok(bankService.viewBalance(bankId));
+    }
+
+    @PostMapping("/bank/withdrawal/{bankId}")
+    public ResponseEntity<User> withdrawal(@PathVariable("bankId") UUID bankId, @RequestBody User user) {
+        return  ResponseEntity.ok().body(bankService.withdraw(bankId,user.getAmount()));
     }
 
 }
