d8e6249a1a39e60c697136034952a45594032389
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-11-22 16:06:25 +0330

    add-test-for BankServiceImpl

---
+++ src/test/java/org/example/bankapp/service/BankServiceImplTest.java
@@ -0,0 +1,29 @@
+package org.example.bankapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void viewBalance() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void transfer() {
+    }
+}
