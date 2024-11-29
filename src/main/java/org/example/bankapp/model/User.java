1a491978218c0a1455de517b39776bad48c604c9
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-11-29 15:40:15 +0330

    User.java add dataBase

--- src/main/java/org/example/bankapp/model/User.java
+++ src/main/java/org/example/bankapp/model/User.java
@@ -1,14 +1,26 @@
 package org.example.bankapp.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "User")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private String username;
     private String password;
     private double amount;
