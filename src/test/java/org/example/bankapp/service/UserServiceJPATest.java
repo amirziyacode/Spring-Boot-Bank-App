1784a8b4fa38376ec0c45fd2381a8a0bef670fec
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-12-18 18:39:11 +0330

    init UserServiceJPATest.java

---
+++ src/test/java/org/example/bankapp/service/UserServiceJPATest.java
@@ -0,0 +1,20 @@
+package org.example.bankapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceJPATest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getTransactions() {
+    }
+
+    @Test
+    void forgetPassword() {
+    }
+}
