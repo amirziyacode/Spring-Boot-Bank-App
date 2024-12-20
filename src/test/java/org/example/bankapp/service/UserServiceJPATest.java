8a40ff7fe4dab7e02b2b21eacdd3929f5e339c3b
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-12-20 22:55:25 +0330

    add get_transactions_bank UserServiceJPATest.java

--- src/test/java/org/example/bankapp/service/UserServiceJPATest.java
+++ src/test/java/org/example/bankapp/service/UserServiceJPATest.java
@@ -14,7 +14,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -61,5 +64,29 @@ class UserServiceJPATest {
         mockMvc.perform(get("/transactions/{id}",user.getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void get_transactions_bank()throws Exception {
+        User user = User.builder()
+                .id(1)
+                .accountNumber(UUID.randomUUID())
+                .username("test")
+                .password("test")
+                .amount(1000)
+                .build();
+        TransactionsBank transactionsBank =  TransactionsBank.builder()
+                .userId(1)
+                .amount(user.getAmount())
+                .accountNumberFrom(user.getAccountNumber())
+                .accountNumberTo(user.getAccountNumber())
+                .createdDate(LocalDateTime.now())
+                .methodName("test")
+                .build();
+
+        given(transactionsBankRepo.findByUserId(user.getId())).willReturn(List.of(transactionsBank));
+        mockMvc.perform(get("/transactions/{id}",user.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
