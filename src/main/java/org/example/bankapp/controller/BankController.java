fa6484f8a7780085ec9e0a13fb9f59a8a0ab9789
Amirziya <75315732+amirziyacode@users.noreply.github.com>
2024-12-22 22:01:53 +0330

    solve It!! BankServiceImplTest

--- src/main/java/org/example/bankapp/controller/BankController.java
+++ src/main/java/org/example/bankapp/controller/BankController.java
@@ -3,7 +3,6 @@ package org.example.bankapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.bankapp.model.User;
-import org.example.bankapp.repo.UserRepository;
 import org.example.bankapp.service.BankService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +15,6 @@ import java.util.UUID;
 public class BankController {
 
     private final BankService bankService;
-    private  final UserRepository userRepository;
 
     @GetMapping("/bank/balance/{bankId}")
     public ResponseEntity<Double> getBalance(@PathVariable("bankId") UUID bankId) {
@@ -25,11 +23,7 @@ public class BankController {
 
     @PostMapping("/bank/withdrawal/{bankId}")
     public ResponseEntity<User> withdrawal(@PathVariable("bankId") UUID bankId, @RequestBody User user) {
-        User byAccountNumber = userRepository.findByAccountNumber(bankId);
-        if(byAccountNumber.getAmount() >= user.getAmount()) {
-            return  ResponseEntity.ok().body(bankService.withdraw(bankId,user.getAmount()));
-        }
-        return  ResponseEntity.status(HttpStatus.FORBIDDEN).body(bankService.withdraw(bankId,user.getAmount()));
+        return  ResponseEntity.ok().body(bankService.withdraw(bankId,user.getAmount()));
     }
 
     @PostMapping("/bank/deposit/{bankId}")
--- src/test/java/org/example/bankapp/service/BankServiceImplTest.java
+++ src/test/java/org/example/bankapp/service/BankServiceImplTest.java
@@ -2,26 +2,18 @@ package org.example.bankapp.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.bankapp.controller.BankController;
-import org.example.bankapp.model.User;
-import org.example.bankapp.repo.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import java.util.Optional;
 import java.util.UUID;
-import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,8 +24,6 @@ class BankServiceImplTest {
     @MockBean
     private BankService bankService;
 
-    @MockBean
-    private UserRepository userRepository;
 
     @Autowired
     MockMvc mockMvc;
@@ -41,8 +31,6 @@ class BankServiceImplTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Captor
-    ArgumentCaptor<UUID> uuidArgumentCaptor;
 
     private BankServiceImpl bankServiceImpl;
 
@@ -70,27 +58,16 @@ class BankServiceImplTest {
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    void withdraw() throws Exception {
-//        given(bankService.withdraw(any(UUID.class),anyDouble())).willReturn(bankServiceImpl.withdraw(bankServiceImpl.user.getAccountNumber(),100.0));
-//        mockMvc.perform(post("/bank/withdrawal/{bankId}",UUID.randomUUID())
-//                        .content("{\"amount\":\"100.0\"}")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//
-//    }
-//    @Test
-//    void withdraw_greater_than_balance() throws Exception {
-//        bankServiceImpl.withdraw(bankServiceImpl.user.getAccountNumber(), 2000.0);
-//        given(bankService.withdraw(any(UUID.class),any(Double.class))).willReturn(bankServiceImpl.user);
-//        mockMvc.perform(post("/bank/withdrawal/{bankId}",bankId)
-//                        .contentType(objectMapper.writeValueAsString(bankServiceImpl.user))
-//                        .content("{\"amount\":\"2000.0\"}")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isForbidden());
-//    }
+    @Test
+    void withdraw() throws Exception {
+        given(bankService.withdraw(any(UUID.class),anyDouble())).willReturn(bankServiceImpl.withdraw(bankServiceImpl.user.getAccountNumber(),100.0));
+        mockMvc.perform(post("/bank/withdrawal/{bankId}",UUID.randomUUID())
+                        .content("{\"amount\":\"100.0\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
     @Test
     void transfer() throws Exception {
         mockMvc.perform(post("/bank/transfer/{bankId}",bankServiceImpl.user.getAccountNumber())
